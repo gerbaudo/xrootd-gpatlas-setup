f565130ba592f5357e052bd8ff60769ea968377e
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2015-10-20 04:19:24 -0700

    also start cmsd (see email Jeff 2015-04-07)

--- stop.sh
+++ stop.sh
@@ -2,4 +2,5 @@
 for x in c-12-15 c-12-19 c-12-23 c-12-27 c-12-31 c-12-35 c-12-39
 do
     ssh -t $x 'sudo -u xrootd bash -c "sudo service xrootd stop"'
+    ssh -t $x 'sudo -u xrootd bash -c "sudo service cmsd stop"'
 done
